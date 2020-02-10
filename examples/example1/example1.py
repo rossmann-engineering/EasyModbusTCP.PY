53d3ea030b207b199a584184c08a0b807ffe7654
sr555 <POeachbarrow793!>
2020-02-10 20:46:21 +0100

    Example1: Connection to Siemens S7-1200

--- examples/example1/example1.py
+++ examples/example1/example1.py
@@ -14,7 +14,7 @@ This is a very common use case of the library to read out data from machines and
 them to cloud platforms.
 
 Used Siemens CPU: CPU 1211C DC/DC/DC (6ES7 211-1AE40-0XB0) - FW: V 4.1.3
-TIA-Portal V16
+Software: TIA-Portal V16
 The Siemens S7 acts as Modbus-TCP Server (IP-Address 192.168.178.110)
 
 Once the Function-Block MB_Server is called we can use Modbus Requests to Read and Write Digital and Analog In-Outs
@@ -24,17 +24,16 @@ Discrete Inputs: 0...8191 mapped to I0.0 to I1023.7
 Input Registers: 0...1021 mapped to IW0 to IW1022
 """
 import easymodbus.modbusClient
-modbus_client = easymodbus.modbusClient.ModbusClient('192.168.178.110', 502)
-modbus_client.connect()
-modbus_client.write_single_register(0,140)                  #We write "140" to the first Holding Register
-modbus_client.write_single_register(1,385)                  #We write "385" to the second Holding Register
-register_values = modbus_client.read_holdingregisters(0,2)  #We Read 2 Holding Registers from the Server
-print ("Value of Register #1:" + str(register_values[0]))    #Should be 140
-print ("Value of Register #2:" + str(register_values[1]))    #Should be 385
-modbus_client.write_single_coil(0,True)                 #We write the First Digital Output to "True"
-print ("Current State of DI0: " + str(modbus_client.read_discreteinputs(0,1)[0]))   #We Read the Current state of the First Digital Input
-
-modbus_client.close()
+modbus_client = easymodbus.modbusClient.ModbusClient('192.168.178.110', 502)    #Create Modbus-TCP class (Server IP-Address and Port)
+modbus_client.connect()                                                         #Connect to the Modbus-TCP Server
+modbus_client.write_single_register(0,140)                                      #We write "140" to the first Holding Register
+modbus_client.write_single_register(1,385)                                      #We write "385" to the second Holding Register
+register_values = modbus_client.read_holdingregisters(0,2)                      #We Read 2 Holding Registers from the Server
+print("Value of Register #1:" + str(register_values[0]))                        #Should be 140
+print("Value of Register #2:" + str(register_values[1]))                        #Should be 385
+modbus_client.write_single_coil(0,True)                                         #We write the First Digital Output to "True"
+print("Current State of DI0: " + str(modbus_client.read_discreteinputs(0,1)[0]))#We Read the Current state of the First Digital Input
+modbus_client.close()                                                           #close socket
 
 
 
